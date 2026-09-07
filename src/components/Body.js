925ce15969c459ea1f6c95d66049de4c7a96c9cc
Barath Kandra <bharathkandra@gmail.com>
2026-09-07 17:40:17 +0530

    implemented api service render data from dynamically

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,80 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantsList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 
-    const [listofRestaurant, setListofRestaurant] = useState(restaurantsList)
+    const [listofRestaurant, setListofRestaurant] = useState([]);
+    const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-    return (
+    const [searchText, setSearchText] = useState("");
+
+    // Add this hook above your return statement
+    useEffect(() => {
+        const filtered = listofRestaurant.filter((res) => 
+          res?.info?.name.toLowerCase().includes(searchText.toLowerCase())
+        );
+      setFilteredRestaurant(filtered);
+    }, [searchText, listofRestaurant]); // Runs whenever searchText or the master list changes
+
+  useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+         const resURl = "https://namastedev.com/api/v1/listRestaurants";
+            const resData = await fetch(resURl);
+
+            const resResponse = await resData.json();
+
+            console.log("resResponse:", resResponse);
+
+            const restaurants =
+            resResponse?.data?.data?.cards[1]?.card?.card
+                ?.gridElements?.infoWithStyle?.restaurants || [];
+
+            console.log("restaurants:", restaurants);
+
+            setListofRestaurant(restaurants);
+            setFilteredRestaurant(restaurants);
+        } catch (error) {
+            console.error("Error fetching restaurants:", error);
+        }
+    };
+
+    return listofRestaurant.length === 0 ? <Shimmer /> : (
         <div className="body">
-            <div className="Filter">
+            <div className="filter">
+                <div className="search">
+                    <input 
+                        type="text" 
+                        className="search-box" 
+                        value={searchText} 
+                        onChange={(e) => {
+                          setSearchText(e.target.value);
+                        }} 
+                    />
+                    <button onClick={ () => {
+                        const filteredRestaurtant = listofRestaurant.filter(
+                            (res) => res?.info.name.toLowerCase().includes(searchText.toLowerCase())
+                        );
+                        setFilteredRestaurant(filteredRestaurtant);
+                    }}>Search</button>
+                </div>
                 <button 
                     className="filter-btn"
                     onClick={ () => {
                         let filteredList = listofRestaurant.filter(
                             (res) => res.info.avgRating > 4.5
                         );
-                        setListofRestaurant(filteredList)
+                        setFilteredRestaurant(filteredList);
                     }}>
                     Top Rated Restaurtant
                 </button>
             </div>
             <div className="res-container">
-                {listofRestaurant.map((restaurtant) => (
+                {filteredRestaurant.map((restaurtant) => (
                     < RestaurantCard key= {restaurtant.info.id} resData={restaurtant}/>
                 ))}
             </div>
